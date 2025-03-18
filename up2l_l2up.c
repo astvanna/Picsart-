60b0c918486365303802341fe251ca11f85505b2
Anna Astvatsatryan <astvanna@gmail.com>
2025-03-18 21:46:01 +0400

    Calculator added

---
+++ up2l_l2up.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+char uppercase(char c) {
+    return c ^ 32;
+}
+
+int main() {
+    char input, output;
+    
+    printf("Enter a character: ");
+    scanf("%c", &input);
+    
+    output = uppercase(input);
+    
+    printf("%c\n", output);
+    
+    return 0;
+}
